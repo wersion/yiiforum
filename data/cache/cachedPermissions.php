abd90b111b0a8074564e5e3c2c09567bb471d831
esion <524135297@qq.com>
2015-06-27 18:04:19 +0800

    c

--- data/cache/cachedPermissions.php
+++ data/cache/cachedPermissions.php
@@ -8,9 +8,16 @@ $cachedPermissionCategories['category_post'] = [
 	'created_at' => '1401452696',
 	'updated_at' => '1401452696',
 	'permissions' => [
+		'1231111',
+		'123123',
+		'222',
+		'2222222222',
+		'2342',
+		'33333333333',
 		'post_add',
 		'post_delete',
 		'post_edit',
+		'sadfasdfasdf',
 	],
 ];
 $cachedPermissionCategories['category_thread'] = [
@@ -27,6 +34,66 @@ $cachedPermissionCategories['category_thread'] = [
 		'thread_edit',
 		'thread_view',
 	],
+];
+$cachedPermissions['1231111'] = [
+	'category' => 'category_post',
+	'name' => '1231111',
+	'type' => 2,
+	'description' => '111111',
+	'rule_name' => '',
+	'data' => '',
+	'created_at' => '1435390583',
+	'updated_at' => '1435390583',
+];
+$cachedPermissions['123123'] = [
+	'category' => 'category_post',
+	'name' => '123123',
+	'type' => 2,
+	'description' => '123123',
+	'rule_name' => '',
+	'data' => '123123',
+	'created_at' => '1435390474',
+	'updated_at' => '1435390474',
+];
+$cachedPermissions['222'] = [
+	'category' => 'category_post',
+	'name' => '222',
+	'type' => 2,
+	'description' => '222222222222',
+	'rule_name' => '',
+	'data' => '22222222222',
+	'created_at' => '1435390592',
+	'updated_at' => '1435390592',
+];
+$cachedPermissions['2222222222'] = [
+	'category' => 'category_post',
+	'name' => '2222222222',
+	'type' => 2,
+	'description' => '22222222222222',
+	'rule_name' => '',
+	'data' => '',
+	'created_at' => '1435389815',
+	'updated_at' => '1435389815',
+];
+$cachedPermissions['2342'] = [
+	'category' => 'category_post',
+	'name' => '2342',
+	'type' => 2,
+	'description' => '3423',
+	'rule_name' => '',
+	'data' => 's:4:"4234";',
+	'created_at' => '1435390540',
+	'updated_at' => '1435390540',
+];
+$cachedPermissions['33333333333'] = [
+	'category' => 'category_post',
+	'name' => '33333333333',
+	'type' => 2,
+	'description' => '333333333333',
+	'rule_name' => '',
+	'data' => '',
+	'created_at' => '1435390741',
+	'updated_at' => '1435390741',
 ];
 $cachedPermissions['post_add'] = [
 	'category' => 'category_post',
@@ -58,6 +125,16 @@ $cachedPermissions['post_edit'] = [
 	'created_at' => '1401455770',
 	'updated_at' => '1401459773',
 ];
+$cachedPermissions['sadfasdfasdf'] = [
+	'category' => 'category_post',
+	'name' => 'sadfasdfasdf',
+	'type' => 2,
+	'description' => 'asdfasdf',
+	'rule_name' => '',
+	'data' => '',
+	'created_at' => '1435388434',
+	'updated_at' => '1435388434',
+];
 $cachedPermissions['thread_add'] = [
 	'category' => 'category_thread',
 	'name' => 'thread_add',
@@ -84,9 +161,9 @@ $cachedPermissions['thread_edit'] = [
 	'type' => 2,
 	'description' => '修改主题',
 	'rule_name' => '',
-	'data' => 's:0:"";',
+	'data' => '',
 	'created_at' => '1401455698',
-	'updated_at' => '1401722495',
+	'updated_at' => '1401455698',
 ];
 $cachedPermissions['thread_view'] = [
 	'category' => 'category_thread',
@@ -94,7 +171,7 @@ $cachedPermissions['thread_view'] = [
 	'type' => 2,
 	'description' => '浏览主题',
 	'rule_name' => '',
-	'data' => 'hhhh',
+	'data' => '',
 	'created_at' => '1401455728',
-	'updated_at' => '1401753005',
+	'updated_at' => '1401455728',
 ];
--- data/cache/cachedRoles.php
+++ data/cache/cachedRoles.php
@@ -8,9 +8,7 @@ $cachedRoleGroups['group_admin'] = [
 	'created_at' => '1401369900',
 	'updated_at' => '1401369900',
 	'roles' => [
-		'admin_level_0',
-		'admin_level_1',
-		'admin_level_2',
+		'5555',
 	],
 ];
 $cachedRoleGroups['group_custom'] = [
@@ -22,8 +20,6 @@ $cachedRoleGroups['group_custom'] = [
 	'created_at' => '1401451311',
 	'updated_at' => '1401451311',
 	'roles' => [
-		'custom_level_0',
-		'custom_level_1',
 	],
 ];
 $cachedRoleGroups['group_member'] = [
@@ -35,13 +31,6 @@ $cachedRoleGroups['group_member'] = [
 	'created_at' => '1401369927',
 	'updated_at' => '1401369927',
 	'roles' => [
-		'member_level_0',
-		'member_level_1',
-		'member_level_2',
-		'member_level_3',
-		'member_level_4',
-		'member_level_5',
-		'member_level_6',
 	],
 ];
 $cachedRoleGroups['group_special'] = [
@@ -53,226 +42,22 @@ $cachedRoleGroups['group_special'] = [
 	'created_at' => '1401369950',
 	'updated_at' => '1401369950',
 	'roles' => [
-		'special_level_0',
-		'special_levle_1',
 	],
 ];
-$cachedRoles['admin_level_0'] = [
+$cachedRoles['5555'] = [
 	'group' => 'group_admin',
-	'name' => 'admin_level_0',
+	'name' => '5555',
 	'type' => 1,
-	'description' => '管理员',
+	'description' => '55555555555555555555',
 	'rule_name' => '',
 	'data' => '',
-	'created_at' => '1401720843',
-	'updated_at' => '1401720843',
+	'created_at' => '1435390978',
+	'updated_at' => '1435390978',
 	'permissions' => [
-		'post_add',
-		'post_delete',
-		'post_edit',
+		'1231111',
+		'123123',
+		'222',
 		'thread_add',
 		'thread_delete',
-		'thread_edit',
-		'thread_view',
-	],
-];
-$cachedRoles['admin_level_1'] = [
-	'group' => 'group_admin',
-	'name' => 'admin_level_1',
-	'type' => 1,
-	'description' => '超级版主',
-	'rule_name' => '',
-	'data' => '',
-	'created_at' => '1401720881',
-	'updated_at' => '1401720881',
-	'permissions' => [
-	],
-];
-$cachedRoles['admin_level_2'] = [
-	'group' => 'group_admin',
-	'name' => 'admin_level_2',
-	'type' => 1,
-	'description' => '版主',
-	'rule_name' => '',
-	'data' => '',
-	'created_at' => '1401720900',
-	'updated_at' => '1401720900',
-	'permissions' => [
-	],
-];
-$cachedRoles['custom_level_0'] = [
-	'group' => 'group_custom',
-	'name' => 'custom_level_0',
-	'type' => 1,
-	'description' => '自定义角色0',
-	'rule_name' => '',
-	'data' => '',
-	'created_at' => '1401720765',
-	'updated_at' => '1401720765',
-	'permissions' => [
-	],
-];
-$cachedRoles['custom_level_1'] = [
-	'group' => 'group_custom',
-	'name' => 'custom_level_1',
-	'type' => 1,
-	'description' => '自定义角色1',
-	'rule_name' => '',
-	'data' => 's:0:"";',
-	'created_at' => '1401720781',
-	'updated_at' => '1401721363',
-	'permissions' => [
-	],
-];
-$cachedRoles['member_level_0'] = [
-	'group' => 'group_member',
-	'name' => 'member_level_0',
-	'type' => 1,
-	'description' => '限制会员',
-	'rule_name' => '',
-	'data' => 's:0:"";',
-	'created_at' => '1401720496',
-	'updated_at' => '1401722061',
-	'permissions' => [
-		'post_delete',
-		'post_edit',
-		'thread_delete',
-	],
-];
-$cachedRoles['member_level_1'] = [
-	'group' => 'group_member',
-	'name' => 'member_level_1',
-	'type' => 1,
-	'description' => '新手上路',
-	'rule_name' => '',
-	'data' => '',
-	'created_at' => '1401720515',
-	'updated_at' => '1401720515',
-	'permissions' => [
-		'post_delete',
-		'post_edit',
-		'thread_add',
-		'thread_delete',
-		'thread_edit',
-		'thread_view',
-	],
-];
-$cachedRoles['member_level_2'] = [
-	'group' => 'group_member',
-	'name' => 'member_level_2',
-	'type' => 1,
-	'description' => '注册会员',
-	'rule_name' => '',
-	'data' => '',
-	'created_at' => '1401720529',
-	'updated_at' => '1401720529',
-	'permissions' => [
-		'post_add',
-		'post_delete',
-		'post_edit',
-		'thread_add',
-		'thread_delete',
-		'thread_edit',
-		'thread_view',
-	],
-];
-$cachedRoles['member_level_3'] = [
-	'group' => 'group_member',
-	'name' => 'member_level_3',
-	'type' => 1,
-	'description' => '中级会员',
-	'rule_name' => '',
-	'data' => '',
-	'created_at' => '1401720551',
-	'updated_at' => '1401720551',
-	'permissions' => [
-		'post_add',
-		'post_delete',
-		'post_edit',
-		'thread_add',
-		'thread_delete',
-		'thread_edit',
-		'thread_view',
-	],
-];
-$cachedRoles['member_level_4'] = [
-	'group' => 'group_member',
-	'name' => 'member_level_4',
-	'type' => 1,
-	'description' => '高级会员',
-	'rule_name' => '',
-	'data' => '',
-	'created_at' => '1401720567',
-	'updated_at' => '1401720567',
-	'permissions' => [
-		'post_add',
-		'post_delete',
-		'post_edit',
-		'thread_add',
-		'thread_delete',
-		'thread_edit',
-		'thread_view',
-	],
-];
-$cachedRoles['member_level_5'] = [
-	'group' => 'group_member',
-	'name' => 'member_level_5',
-	'type' => 1,
-	'description' => '金牌会员',
-	'rule_name' => '',
-	'data' => 's:0:"";',
-	'created_at' => '1401720580',
-	'updated_at' => '1401801898',
-	'permissions' => [
-		'post_add',
-		'post_delete',
-		'post_edit',
-		'thread_add',
-		'thread_delete',
-		'thread_edit',
-		'thread_view',
-	],
-];
-$cachedRoles['member_level_6'] = [
-	'group' => 'group_member',
-	'name' => 'member_level_6',
-	'type' => 1,
-	'description' => '论坛元老',
-	'rule_name' => '',
-	'data' => '',
-	'created_at' => '1401720596',
-	'updated_at' => '1401720596',
-	'permissions' => [
-		'post_add',
-		'post_delete',
-		'post_edit',
-		'thread_add',
-		'thread_delete',
-		'thread_edit',
-		'thread_view',
-	],
-];
-$cachedRoles['special_level_0'] = [
-	'group' => 'group_special',
-	'name' => 'special_level_0',
-	'type' => 1,
-	'description' => '特殊角色0',
-	'rule_name' => '',
-	'data' => '',
-	'created_at' => '1401720708',
-	'updated_at' => '1401720708',
-	'permissions' => [
-	],
-];
-$cachedRoles['special_levle_1'] = [
-	'group' => 'group_special',
-	'name' => 'special_levle_1',
-	'type' => 1,
-	'description' => '特殊角色1',
-	'rule_name' => '',
-	'data' => '',
-	'created_at' => '1401720727',
-	'updated_at' => '1401720727',
-	'permissions' => [
 	],
 ];
